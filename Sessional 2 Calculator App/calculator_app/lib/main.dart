39b23e85747241988876cb5ba0dbe1b8e8765f7e
Ibtesam Ahmad <80882005+ibtesam3@users.noreply.github.com>
2021-05-08 09:34:22 -0700

    3rd commit: Add Hard and Simple option,  6th commit: go home from any screen,   8th commit: Navigation drawer functionality,   9th commit: Contact us screen

--- Sessional 2 Calculator App/calculator_app/lib/main.dart
+++ Sessional 2 Calculator App/calculator_app/lib/main.dart
@@ -72,6 +72,46 @@ class _HomeState extends State<Home> {
           ),
           backgroundColor: Colors.blueGrey,
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                hoverColor: Colors.blueGrey,
+                title: Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                },
+              ),
+              ListTile(
+                hoverColor: Colors.blueGrey,
+                title: Text(
+                  'Contact Us',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ContactUs()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -80,6 +120,203 @@ class _HomeState extends State<Home> {
               child: CircleAvatar(
                 radius: 150.0,
                 backgroundImage: AssetImage('Images/calculator_image.jpg'),
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            FlatButton(
+              hoverColor: Colors.blueGrey,
+              onPressed: () {
+                setState(() {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                      builder: (context) => new MaterialApp(),
+                    ),
+                  );
+                });
+              },
+              color: Colors.blueGrey[900],
+              textColor: Colors.black,
+              child: Text(
+                'Simple',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 37.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            FlatButton(
+              hoverColor: Colors.blueGrey,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                    builder: (context) => new MaterialApp(),
+                  ),
+                );
+              },
+              color: Colors.blueGrey[900],
+              textColor: Colors.black,
+              child: Text(
+                'Hard',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 37.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Contact Us',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40.0,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              hoverColor: Colors.blueGrey,
+              title: Text(
+                'Home',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                );
+              },
+            ),
+            ListTile(
+              hoverColor: Colors.blueGrey,
+              title: Text(
+                'Contact Us',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ContactUs()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('Images/i.jpg'),
+              backgroundColor: Colors.blueGrey,
+              radius: 150.0,
+            ),
+            Text(
+              'Ibtesam Ahmad',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+            Text(
+              'BSCs Student',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+                letterSpacing: 5.0,
+              ),
+            ),
+            Card(
+              color: Colors.blueGrey,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                title: Text(
+                  '+923061793107',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blueGrey,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                title: Text(
+                  'ibtesamahmad75@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blueGrey,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                title: Text(
+                  'Vehari',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ],
